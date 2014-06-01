61165ef502c305d6e0e84eeb65382336ea0e534b
Mauricio Fernandes <mauriciofernandes1991@gmail.com>
2014-06-01 13:53:55 -0300

    teste1

--- server.R
+++ server.R
@@ -2,7 +2,7 @@ palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
           "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 require(RCurl)
 #source(getURL("https://raw.githubusercontent.com/Recca2012/mapas/master/mapas.R"))
-u <- "https://raw.githubusercontent.com/Recca2012/mapas/master/mapas.R"
+u <- "https://raw.githubusercontent.com/Recca2012/grupos/master/mapas.R"
 script <- getURL(u, ssl.verifypeer = FALSE)
 eval(parse(text = script))
 
